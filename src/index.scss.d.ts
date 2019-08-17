5199cd1c5e8a89d53d278b86e6161179b61c2d64
GQSM <ms314006@hotmail.com>
2019-08-18 01:16:33 +0800

    [ADD] initial project

---
+++ src/index.scss.d.ts
@@ -0,0 +1,3 @@
+export const mainBlock: string;
+export const wrongText: string;
+export const image: string;
---
+++ src/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState, } from 'react';
+import ReactDOM from 'react-dom';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styles from './index.scss';
+
+const Main = () => {
+  const [password, setPassword] = useState('');
+  const [displayUrl, setDisplayUrl] = useState(false);
+  const [wrongMessage, setWrongMessage] = useState(false);
+  const submitPassword = () => {
+    const allowPassword = ['蘋果', '香蕉', 'apple', 'banana'];
+    if (allowPassword.indexOf(password) !== -1) {
+      setDisplayUrl(true);
+    } else {
+      setWrongMessage(true);
+    }
+  }
+  return (
+    <div className={styles.mainBlock}>
+      {
+        !displayUrl ? (
+        <div className={styles.mainBlock}>
+          <span
+            className={styles.wrongText}
+            style={{ display: wrongMessage ? 'inline-block' : 'none',}}
+          >
+            通關密語輸入錯誤！
+          </span>
+          <TextField
+            label="Password"
+            margin="normal"
+            value={password}
+            onChange={(e) => {setPassword(e.target.value)}}
+          />
+          <div>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={submitPassword}
+            >
+              確定送出
+            </Button>
+          </div>
+        </div>) : (
+        <a href="https://unsplash.com/photos/5bYxXawHOQg">
+          <div
+            className={styles.image}
+            style={{
+              backgroundImage: `url(https://images.unsplash.com/photo-1517842645767-c639042777db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80)`
+            }}
+          />
+        </a>
+        )
+      }
+      
+    </div>
+  );
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
